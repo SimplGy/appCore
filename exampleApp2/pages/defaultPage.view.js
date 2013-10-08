99265862df625fcd4538c2cba112f17fa55458a6
Eric <Eric@SimpleAsCouldBe.com>
2013-10-08 13:50:55 -0700

    Adding exampleApp2. Sometimes It's helpful to be able to see two instances of something.

---
+++ exampleApp2/pages/defaultPage.view.js
@@ -0,0 +1,33 @@
+/*globals define */
+/*jshint asi:true */
+
+
+define(
+    [
+        'backbone',
+        'app',
+        'dolla'
+    ],
+    function(Backbone, app, $) {
+        var me = 'exampleApp2/pages/defaultPage.view',
+            Page
+
+
+        Page = Backbone.View.extend({
+            /**
+             * Set up a new view
+             */
+            initialize: function() {
+                $(this.options.parentSelector).html(this.$el)
+                this.render()
+            },
+            render: function() {
+                this.$el.html(me)
+                this.$el.append('<br/> options: ' + this.options.urlParams[0] +' / '+ this.options.urlParams[1] +' / '+ this.options.urlParams[2] )
+                app.trigger('pageLoaded:'+app.model.get('curPage').id)
+                $('body').css('background-color','lawngreen')
+            }
+        })
+        return Page
+    }
+)
